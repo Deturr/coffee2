4258172329b7625fdac62092cd4de3708645dcd7
Toby Emeboh <Toby.Emeboh@student.rocva.nl>
2024-11-04 20:10:51 +0100

    Commit 1 DD

---
+++ views/bestelling/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Bestelling $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="bestelling-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'medewerker_id')->dropDownList([ '1'=> 'Ayoub', '2'=> 'Brahim','3'=> 'Carla','4'=> 'Diego','5'=> 'Eisa' ], ['prompt' => '']) ?>
+
+    <?= $form->field($model, 'naam')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'menu_id')->textInput() ?>
+
+    <?= $form->field($model, 'status')->dropDownList([ 'besteld' => 'Besteld', 'klaar' => 'Klaar', 'geleverd' => 'Geleverd', ], ['prompt' => '']) ?>
+
+    <!--<?= $form->field($model, 'timestamp')->textInput() ?> -->
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+<?php // Toby Emeboh?>
+</div>
